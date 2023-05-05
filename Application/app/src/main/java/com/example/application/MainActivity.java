54944a9ca56ae7c69d285a2e87dcc0aef2642bb6
sanken5 <2368338572@qq.com>
2023-05-05 10:25:54 +0800

    first commit

---
+++ Application/app/src/main/java/com/example/application/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText name = (EditText)findViewById(R.id.name);
+        final EditText Password1 = (EditText)findViewById(R.id.pwd1);
+        final EditText Password2 = (EditText)findViewById(R.id.pwd2);
+        //性别RadioGroup
+        final RadioGroup sex = (RadioGroup)findViewById(R.id.rgSex);
+        //爱好CheckBox
+        final CheckBox like1 = (CheckBox)findViewById(R.id.like1);
+        final CheckBox like2 = (CheckBox)findViewById(R.id.like2);
+        final CheckBox like3 = (CheckBox)findViewById(R.id.like3);
+
+        Button btn1 = (Button) findViewById(R.id.btn);
+
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,SecondActivity.class);
+                Bundle bundle=new Bundle();
+                bundle.putCharSequence("name", name.getText().toString()); //用户名
+                bundle.putCharSequence("pwd1", Password1.getText().toString());
+                bundle.putCharSequence("pwd2", Password2.getText().toString());
+
+                //性别的输出
+                for (int i = 0; i < sex.getChildCount(); i++) {
+                    RadioButton r = (RadioButton) sex.getChildAt(i);
+                    if (r.isChecked()) {
+                        bundle.putCharSequence("sex", r.getText().toString());
+                        break;            //跳出for循环
+                    }
+                }
+
+                String str = "";                //保存选中的值
+                // 爱好
+                if (like1.isChecked())            //当第一个复选按钮被选中
+                    str += like1.getText().toString() + "  ";
+                if (like2.isChecked())            //当第一个复选按钮被选中
+                    str += like2.getText().toString() + "  ";
+                if (like3.isChecked())            //当第一个复选按钮被选中
+                    str += like3.getText().toString() + "  ";
+
+                bundle.putCharSequence("like", str);
+
+                intent.putExtras(bundle);//将Bundle对象添加到Intent对象中
+                startActivity(intent);
+
+            }
+        });
+
+    }
+}
---
+++ Application/app/src/main/java/com/example/application/SecondActivity.java
@@ -0,0 +1,31 @@
+package com.example.application;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_main);
+        final TextView user = (TextView) findViewById(R.id.name);
+        final TextView pwd1 = (TextView) findViewById(R.id.pwd1);
+        final TextView pwd2 = (TextView) findViewById(R.id.pwd2);
+        final TextView sex = (TextView) findViewById(R.id.sex);
+        final TextView like = (TextView) findViewById(R.id.like);
+
+        Intent intent=getIntent(); //获取Intent对象
+        Bundle bundle= ((Intent) intent).getExtras();  //获取传递的数据包
+        user.setText("用户名："+bundle.getString("name"));
+        pwd1.setText("密   码："+bundle.getString("pwd1"));
+        pwd2.setText("确认密码："+bundle.getString("pwd2"));
+        sex.setText("性    别："+bundle.getString("sex"));
+        like.setText("爱    好："+bundle.getString("like"));
+
+
+    }
+}
